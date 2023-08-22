847f8126e8469b99590bd23e8b3b240a9676beec
andres novio <andresnov@hotmail.com>
2023-08-22 20:08:59 -0300

    primera versión

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+const Navbar = () => {
+    return (
+    <nav>
+      <ul>
+        <li>
+          Guitarras
+        </li>
+        <li>
+          Cuerda frotada
+        </li>
+        <li>
+          Percusión
+        </li>
+        <li>
+          Vientos
+        </li>
+      </ul>
+    </nav>
+    )
+  }
+
+  export default Navbar
