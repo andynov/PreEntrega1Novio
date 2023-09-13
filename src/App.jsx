48fbf2d4c50d25626769026c93b3ed3d350e859a
andres novio <andresnov@hotmail.com>
2023-09-13 08:37:16 -0300

    mejoras en routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import NavBar from './components/Navbar'
 import ItemListContainer from './components/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
@@ -9,14 +11,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
-    <>
       <div>
-        <NavBar />
-        <ItemListContainer />
-        <ItemDetailContainer /> 
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />} /> 
+            <Route path='/detalle' element={<ItemDetailContainer />} /> 
+          </Routes>
+        </Router> 
       </div>
-    </>
-  )
+ )
 }
 
 export default App
